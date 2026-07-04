251aa575749be5c2830f9727a76cec3d5f9f6370
chibigg4-lang <chibigg4@gmail.com>
2026-07-05 01:53:01 +0700

    Add stateless LLM generation chain using ChatPromptTemplate

---
+++ src/generation/llm_chain.py
@@ -0,0 +1,16 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+def create_chain():
+    llm = ChatGoogleGenerativeAI(
+    model = "gemini-1.5-flash",
+    temperature = 0.7,
+    )
+    template_setting = "You are a helpful assistant. Answer the user's question using ONLY the provided context. If the answer is not in the context, explicitly say 'I don't know'."
+    template = ChatPromptTemplate.from_messages([
+        ("system", template_setting),
+        ("human", "Context: {context}\nQuestion: {question}\n")
+    ])
+    chain = template | llm | StrOutputParser()
+    return chain
