aeb6bab2502360864cdcfa2c2edde309dc8da066
chibigg4-lang <chibigg4@gmail.com>
2026-07-04 21:36:10 +0700

    Initial commit: Set up RAG modular architecture and vector storage

--- New folder/Vector_store.py
+++ New folder/Vector_store.py
@@ -1,7 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import Chroma
-from ingestion.splitters import chunks
-embedding_model = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
-vector_store = Chroma("my_collection", embedding_model)
-for chunk in chunks:
-    vector_store.add_documents([doc])
+from langchain_community.vectorstores import Chroma
+
+def building_vector_store(chunks):
+    embedding_model = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
+    vector_store = Chroma.from_documents(
+        documents=chunks, 
+        embedding=embedding_model, 
+        collection_name="my_collection"
+    )
+    return vector_store
